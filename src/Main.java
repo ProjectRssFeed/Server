f8667b87516e0e9b8a51068feef948d106f9d9a8
Jean-Yves Bateman <jeanyves.bateman@gmail.com>
2017-01-24 04:45:09 +0900

    Added rss manager class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        SQLConnect conn = new SQLConnect();
+        RSSMan rss = new RSSMan();
     }
 }
---
+++ src/RSSMan.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Spoony on 24/01/2017.
+ */
+public class RSSMan {
+    private  SQLConnect conn;
+
+    public RSSMan() {
+        SQLConnect conn = new SQLConnect();
+    }
+}
