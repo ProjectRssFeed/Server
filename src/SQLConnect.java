bcac7c6c12b77c276abd21ccfdaf57412c203a63
Jean-Yves Bateman <jeanyves.bateman@gmail.com>
2017-01-25 09:11:06 +0900

    Create database if doesn't exist

--- src/SQLConnect.java
+++ src/SQLConnect.java
@@ -3,22 +3,38 @@
  */
 
 import java.lang.reflect.Executable;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class SQLConnect {
     private Connection conn;
     private Statement st;
     private ResultSet rs;
 
+    private String dbName = "rss";
+    private String user = "root";
+    private String password = "helloworld";
 
     public SQLConnect() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rssfeed", "root", "helloworld");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", this.user, this.password);
             st = conn.createStatement();
+            st.executeUpdate("CREATE DATABASE "+this.dbName);
+        } catch(SQLException sqlex) {
+            if (sqlex.getErrorCode() == 1007) {
+                try {
+                    conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+this.dbName, this.user, this.password);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    st = conn.createStatement();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                sqlex.printStackTrace();
+            }
         } catch(Exception ex) {
             System.out.println("Error:"+ex);
         }
